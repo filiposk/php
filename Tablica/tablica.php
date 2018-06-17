af8f29d9d18a56ccd498fdd0fd8b59398b807b71
DESKTOP-B2IB0NK\Zla Paprika <fpavlovic1995@gmail.com>
2018-06-17 12:35:06 +0200

    Tablica ispisuje brojeve redom

--- Tablica/tablica.php
+++ Tablica/tablica.php
@@ -1,6 +1,9 @@
 <?php
      $broj1 = isset($_GET["broj1"])? $_GET["broj1"]:0;
      $broj2 = isset($_GET["broj2"])? $_GET["broj2"]:0;
+     $counter = 1 ;
+
+     
 ?>
 
 <!doctype html>
@@ -38,16 +41,23 @@
 
      <table class="tablica">
           <tbody> 
+        
             <?php
+            
             for($i=0;$i<$broj1;$i++)
             {
+            
               echo "<tr>";
               for($j=0;$j<$broj2;$j++)
               {
-                echo"<td>" . "x" . "</td>"; //+1 zato da ne ispisuje nule!
+                 if($counter <= ($broj1*$broj2)){
+                echo"<td>".$counter."</td>";
+                    $counter++ ;    
+            }
               }
             echo"</tr>";  
             }
+            
             ?>
           </tbody>
       </table>
